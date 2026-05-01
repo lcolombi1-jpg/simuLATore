cbb8a4d0a25f93bcb551d3f95d0cf8327ab6faff
lcolombi1-jpg <l.colombi1@student.unisi.it>
2026-05-01 12:20:05 +0200

    Remove color attribute from sidebar image

--- lat.friendly.py
+++ lat.friendly.py
@@ -14,7 +14,7 @@ st.markdown("""
 
 # 3. Sidebar per info e progresso
 with st.sidebar:
-    st.image("https://img.icons8.com/ios/100/2ecc71/coliseum.png", width=100, color="black")
+    st.image("https://img.icons8.com/ios/100/2ecc71/coliseum.png", width=100)
     st.title("Statistiche Quiz")
     st.info("Benvenuto! Rispondi a tutte le domande per testare la tua conoscenza del Latino.")
     
